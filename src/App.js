221b769ed31f85cd323c7c580a62214c5f880646
beperello <beperello@DESKTOP-1F1B6LN.localdomain>
2022-11-03 21:02:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import image from './Dfinity_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={image} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
